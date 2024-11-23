87355abb084936e665299fea75e3ae68c56e38ae
ALICE150699 <alice_alife80@hotmail.com>
2024-11-23 16:00:09 +0000

    aula2

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+gifrom flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def pagina_inicial():
+    return render_template("pagina_inicial.html")
+
+@app.route('/contato')
+def contato():
+    return render_template("contato.html")
+
+@app.route('/msgenviada')
+def msgenviada():
+    return render_template("msgenviada.html")
+
+@app.route('/rota_filha')
+def rotafilha():
+    return render_template("rota_filha.html")
